a3d315189438781b29d214b6d1cf04b1b8b31fdc
Alexandra Huliakevich <sunnykuzia1996@gmail.com>
2022-08-06 19:26:09 +0300

    feat: implement reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let str = "";
+
+    if (n < 0) {
+        str = n.toString().slice(1);
+    } else {
+        str = n.toString();
+    }
+
+    let arr = str.split("").reverse();
+    return parseInt((arr.join("")));
 }
